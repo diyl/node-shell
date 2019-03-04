3e414caedfd938ef2ef9a2033ae1d6915f0fa10d
perlows <betzalel@perlow.me>
2019-03-04 12:40:04 -0500

    completed pwd

---
+++ bash.js
@@ -0,0 +1,13 @@
+const pwd = require('./pwd');
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
+
+  process.stdout.write('You typed: ' + cmd);
+
+  if (cmd === 'pwd') {
+    pwd();
+  }
+});
